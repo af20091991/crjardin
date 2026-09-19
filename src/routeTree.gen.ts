bad1f14fae1e20ddd2850d7edc94d3058b7233e3
af20091991 <fournier.anthony2009@gmail.com>
2026-09-19 10:35:41 +0200

    feat(premium): register Clients Premium route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -32,6 +32,7 @@ import { Route as AuthenticatedParcMaterielIndexRouteImport } from './routes/_au
 import { Route as AuthenticatedInterventionsIndexRouteImport } from './routes/_authenticated/interventions.index'
 import { Route as AuthenticatedFichesIndexRouteImport } from './routes/_authenticated/fiches.index'
 import { Route as AuthenticatedClientsIndexRouteImport } from './routes/_authenticated/clients.index'
+import { Route as AuthenticatedClientsPremiumRouteImport } from './routes/_authenticated/clients.premium'
 import { Route as LovableEmailEventsRouteImport } from './routes/lovable/email/events'
 import { Route as ApiPublicEmailOpenRouteImport } from './routes/api/public/email-open'
 import { Route as AuthenticatedPilotValidationRouteImport } from './routes/_authenticated/pilot.validation'
@@ -195,6 +196,11 @@ const AuthenticatedClientsIndexRoute =
     path: '/clients/',
     getParentRoute: () => AuthenticatedRouteRoute,
   } as any)
+const AuthenticatedClientsPremiumRoute = AuthenticatedClientsPremiumRouteImport.update({
+  id: '/clients/premium',
+  path: '/clients/premium',
+  getParentRoute: () => AuthenticatedRouteRoute,
+} as any)
 const LovableEmailEventsRoute = LovableEmailEventsRouteImport.update({
   id: '/lovable/email/events',
   path: '/lovable/email/events',
@@ -457,6 +463,7 @@ export interface FileRoutesByFullPath {
   '/versions': typeof AuthenticatedVersionsRoute
   '/partage/$token': typeof PartageTokenRoute
   '/clients/$clientId': typeof AuthenticatedClientsClientIdRoute
+  '/clients/premium': typeof AuthenticatedClientsPremiumRoute
   '/clients/$clientId/premium': typeof AuthenticatedClientsClientIdPremiumRoute
   '/fiches/$ficheId': typeof AuthenticatedFichesFicheIdRoute
   '/fiches/new': typeof AuthenticatedFichesNewRoute
@@ -492,6 +499,7 @@ export interface FileRoutesByFullPath {
   '/api/public/email-open': typeof ApiPublicEmailOpenRoute
   '/lovable/email/events': typeof LovableEmailEventsRoute
   '/clients/': typeof AuthenticatedClientsIndexRoute
+  '/clients/premium': typeof AuthenticatedClientsPremiumRoute
   '/fiches/': typeof AuthenticatedFichesIndexRoute
   '/interventions/': typeof AuthenticatedInterventionsIndexRoute
   '/parc-materiel/': typeof AuthenticatedParcMaterielIndexRoute
@@ -1417,6 +1425,7 @@ interface AuthenticatedRouteRouteChildren {
   AuthenticatedInterventionsNewRoute: typeof AuthenticatedInterventionsNewRoute
   AuthenticatedParcMaterielEquipmentIdRoute: typeof AuthenticatedParcMaterielEquipmentIdRoute
   AuthenticatedClientsIndexRoute: typeof AuthenticatedClientsIndexRoute
+  AuthenticatedClientsPremiumRoute: typeof AuthenticatedClientsPremiumRoute
   AuthenticatedFichesIndexRoute: typeof AuthenticatedFichesIndexRoute
   AuthenticatedInterventionsIndexRoute: typeof AuthenticatedInterventionsIndexRoute
   AuthenticatedParcMaterielIndexRoute: typeof AuthenticatedParcMaterielIndexRoute
@@ -1445,6 +1454,7 @@ const AuthenticatedRouteRouteChildren: AuthenticatedRouteRouteChildren = {
   AuthenticatedParcMaterielEquipmentIdRoute:
     AuthenticatedParcMaterielEquipmentIdRoute,
   AuthenticatedClientsIndexRoute: AuthenticatedClientsIndexRoute,
+  AuthenticatedClientsPremiumRoute: AuthenticatedClientsPremiumRoute,
   AuthenticatedFichesIndexRoute: AuthenticatedFichesIndexRoute,
   AuthenticatedInterventionsIndexRoute: AuthenticatedInterventionsIndexRoute,
   AuthenticatedParcMaterielIndexRoute: AuthenticatedParcMaterielIndexRoute,
