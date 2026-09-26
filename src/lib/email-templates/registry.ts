b76b85ecbc0b0c63198029272adb4ff718517cbf
af20091991 <fournier.anthony2009@gmail.com>
2026-09-26 16:27:48 +0200

    Enregistre le modèle email des fiches SST

--- src/lib/email-templates/registry.ts
+++ src/lib/email-templates/registry.ts
@@ -24,10 +24,12 @@ import { template as newReportTemplate } from "./new-report";
 import { template as clientActivityTemplate } from "./client-activity";
 import { template as sstPlanningTemplate } from "./sst-planning";
 import { template as apReminderTemplate } from "./ap-reminder";
+import { template as sstWorksiteSheetTemplate } from "./sst-worksite-sheet";
 
 export const TEMPLATES: Record<string, TemplateEntry> = {
   "new-report": newReportTemplate,
   "client-activity": clientActivityTemplate,
   "sst-planning": sstPlanningTemplate,
   "ap-reminder": apReminderTemplate,
+  "sst-worksite-sheet": sstWorksiteSheetTemplate,
 };
